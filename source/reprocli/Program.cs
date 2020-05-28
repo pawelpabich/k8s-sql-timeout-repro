f0372cacfbb0be03d83e2a3cabb952b3b204ff2d
Pawel Pabich <pawel@pabich.eu>
2020-05-28 14:17:12 +1000

    Compare both

--- source/reprocli/Program.cs
+++ source/reprocli/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
-using Microsoft.Data.SqlClient;
 
 namespace reprocli
 {
@@ -21,8 +14,21 @@ namespace reprocli
                 var path = args[0];
 
                 Console.WriteLine($"Path: {path}");
-                var driveInfo = new DriveInfo(Directory.GetDirectoryRoot(path));
-                Console.WriteLine($"Free Space: {driveInfo.AvailableFreeSpace / 1024L / 1024L / 1024L} GB, Format: {driveInfo.DriveFormat} Name: {driveInfo.Name}");
+
+
+                var stopwatch = Stopwatch.StartNew();
+                    var driveInfo = new DriveInfo(Directory.GetDirectoryRoot(path));
+                    Console.WriteLine($"Free Space: {driveInfo.AvailableFreeSpace / 1024L / 1024L / 1024L} GB, Format: {driveInfo.DriveFormat} Name: {driveInfo.Name}");
+                stopwatch.Stop();
+                Console.WriteLine("Time: " + stopwatch.Elapsed);
+
+                var stopwatchDir = Stopwatch.StartNew();
+                    var size = CaculateSize(new DirectoryInfo(path));
+                    Console.WriteLine("Size: " + (size / 1024L / 1024L + " MB"));
+                stopwatchDir.Stop();
+                Console.WriteLine("Time: " + stopwatchDir.Elapsed);
+
+
 
             }
             catch (Exception e)
@@ -33,6 +39,17 @@ namespace reprocli
             Console.ReadLine();
         }
 
+        private static long CaculateSize(DirectoryInfo directoryInfo)
+        {
+            Console.WriteLine("Caclulation size for " + directoryInfo.FullName);
 
+            var files = directoryInfo.GetFiles();
+            var dirs = directoryInfo.GetDirectories();
+
+            var size = files.Select(f => f.Length).Sum();
+
+            return size + dirs.Select(CaculateSize).Sum();
+
+        }
     }
 }
